08d90c6969425d4396c30993918178f5c29435c5
davelpat <dpatterson@monsooncommerce.com>
2014-04-06 10:43:01 -0700

    added tests for short term parking lot

--- features/step_definitions/parking_lot_steps.rb
+++ features/step_definitions/parking_lot_steps.rb
@@ -1,4 +1,4 @@
-When(/^I park my car in the (\w+ Parking) Lot for (.*)$/) do |lot, duration|
+When(/^I park my car in the ([\w-]+ Parking) Lot for (.*)$/) do |lot, duration|
   $parkcalc.select_location(lot)
   $parkcalc.enter_parking_duration(duration)
 end
@@ -6,3 +6,5 @@ end
 Then(/^I will have to pay (.*)$/) do |expected_price|
   $parkcalc.calculated_fee.should == expected_price
 end
+
+
