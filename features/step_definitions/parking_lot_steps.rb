558cc2b9a9c5d872cfa7eed2da65772a64c9e788
davelpat <dpatterson@monsooncommerce.com>
2014-04-05 23:59:28 -0700

    refactored valet lot to be general

--- features/step_definitions/parking_lot_steps.rb
+++ features/step_definitions/parking_lot_steps.rb
@@ -1,5 +1,5 @@
-When(/^I park my car in the Valet Parking Lot for (.*)$/) do |duration|
-  $parkcalc.select_location('Valet Parking')
+When(/^I park my car in the (\w+ Parking) Lot for (.*)$/) do |lot, duration|
+  $parkcalc.select_location(lot)
   $parkcalc.enter_parking_duration(duration)
 end
 
